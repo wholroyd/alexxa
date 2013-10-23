259dd8cef9b9db8137649275b783cfd4884c2754
versity <wholroyd@gmail.com>
2013-10-23 17:02:47 -0400

    Initial commit.

---
+++ Alexxa.Examples/ExamplesWithData.cs
@@ -0,0 +1,38 @@
+﻿namespace Alexxa.Examples
+{
+    using System.Data;
+    using System.Data.SqlClient;
+
+    using Alexxa.Core.Data;
+
+    public class ExamplesWithData
+    {
+        public string ExampleWithSql()
+        {
+            using (var sqlCon = new SqlConnection("server=prodsql01;catalog=Datastore;integrated security=true"))
+            using (var sqlCmd = sqlCon.CreateCommand())
+            {
+                sqlCmd.CommandText = "SELECT TOP 1 FullName FROM People ORDER BY Age DESC";
+                sqlCmd.CommandTimeout = 30;
+                sqlCmd.CommandType = CommandType.Text;
+
+                return sqlCmd.ExecuteScalar() as string;
+            }
+        }
+
+        public string ExampleWithAlexxa()
+        {
+            using (var alxCon = new AlexxaConnection("deployment=Datastore"))
+            using (var alxCmd = alxCon.CreateCommand())
+            {
+                alxCmd.CommandText = "SELECT TOP 1 FullName FROM People ORDER BY Age DESC";
+                alxCmd.CommandTimeout = 30;
+                alxCmd.CommandType = CommandType.Text;
+
+                alxCmd.Partition = AlexxaPartitionService.GetPartition(1982);
+
+                return alxCmd.ExecuteScalar() as string;
+            }
+        }
+    }
+}
---
+++ Alexxa.Examples/ExamplesWithManagement.cs
@@ -0,0 +1,58 @@
+﻿namespace Alexxa.Examples
+{
+    using System;
+    using System.Collections.Specialized;
+
+    using Microsoft.SqlServer.Management.Smo;
+
+    public class ExamplesWithManagement
+    {
+        public void ExampleWithSql()
+        {
+            var sqlServer = new Microsoft.SqlServer.Management.Smo.Server();
+            var sqlDatabase = sqlServer.Databases[0];
+            sqlDatabase.Alter();
+            sqlDatabase.Alter(TerminationClause.FailOnOpenTransactions);
+            sqlDatabase.Alter(new TimeSpan());
+            sqlDatabase.ChangeMirroringState(MirroringOption.Failover);
+            StringCollection allocations = sqlDatabase.CheckAllocations(RepairType.AllowDataLoss);
+            StringCollection allocationsData = sqlDatabase.CheckAllocationsDataOnly();
+            var sqlTable = sqlDatabase.Tables[0];
+            var sqlColumn = sqlTable.Columns[0];
+            sqlColumn.AddDefaultConstraint();
+            sqlColumn.Alter();
+            sqlColumn.BindDefault(string.Empty, string.Empty);
+            sqlColumn.BindRule(string.Empty, string.Empty);
+            sqlColumn.Create();
+            sqlColumn.Discover();
+            sqlColumn.Drop();
+            sqlColumn.EnumForeignKeys();
+            sqlColumn.EnumIndexes();
+            sqlColumn.EnumUserPermissions(string.Empty);
+        }
+        
+        public void ExampleWithAlexxa()
+        {
+            var alxServer = new Core.Management.Amo.Server();
+            var alxDatabase = alxServer.Databases[0];
+            alxDatabase.Alter();
+            alxDatabase.Alter(TerminationClause.FailOnOpenTransactions);
+            alxDatabase.Alter(new TimeSpan());
+            alxDatabase.ChangeMirroringState(MirroringOption.Failover);
+            StringCollection allocations = alxDatabase.CheckAllocations(RepairType.AllowDataLoss);
+            StringCollection allocationsData = alxDatabase.CheckAllocationsDataOnly();
+            var alxTable = alxDatabase.Tables[0];
+            var alxColumn = alxTable.Columns[0];
+            alxColumn.AddDefaultConstraint();
+            alxColumn.Alter();
+            alxColumn.BindDefault(string.Empty, string.Empty);
+            alxColumn.BindRule(string.Empty, string.Empty);
+            alxColumn.Create();
+            alxColumn.Discover();
+            alxColumn.Drop();
+            alxColumn.EnumForeignKeys();
+            alxColumn.EnumIndexes();
+            alxColumn.EnumUserPermissions(string.Empty);
+        }
+    }
+}
